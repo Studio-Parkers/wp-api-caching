a063c3b03247c2c201db334d5da26f3d0c6f6358
danivdwerf <danivdwerf@gmail.com>
2023-11-02 13:08:40 +0100

    Setup relations dropdowns

--- admin/javascript/options.js
+++ admin/javascript/options.js
@@ -12,13 +12,61 @@ const toggleFieldset = (event)=>
     fieldset.setAttribute("collapsed", !collapsed);
 };
 
+const toggleRelatedPosts = (event)=>
+{
+    const container = event.target.nextElementSibling.nextElementSibling;
+    container.style.display = event.target.checked ? "block" : "none";
+};
+
+const removeRelation = (event)=>
+{
+    const row = event.target.parentElement;
+    row.parentElement.removeChild(row);
+};
+
+const addRelation = (event)=>
+{
+    const parent = event.target.parentElement;
+
+    const container = document.createElement("div");
+    container.className = "post-type-row";
+    parent.appendChild(container);
+
+    const select = document.createElement("select");
+    select.name = `${parent.getAttribute("data-hash")}_relations[]`;
+    container.appendChild(select);
+    postTypes.forEach(postType=>
+    {
+        const option = document.createElement("option");
+        option.innerHTML = postType;
+        select.appendChild(option);
+    });
+
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.innerHTML = "remove";
+    btn.addEventListener("click", removeRelation);
+    container.appendChild(btn);
+};
+
 /**
  * Add click event to legend.
  * 
  * @param {HTMLFieldSetElement} fieldset 
  */
 const setupEventListener = (fieldset)=>
+{
     fieldset.firstElementChild.addEventListener("click", toggleFieldset);
+    fieldset.querySelectorAll("input[type='checkbox']").forEach(input=>
+    {
+        const relatedContainer = input.nextElementSibling.nextElementSibling;
+        relatedContainer.setAttribute("collapsed", true);
+        relatedContainer.querySelector("button").addEventListener("click", addRelation);
+        
+        input.addEventListener("change", toggleRelatedPosts);
+        input.dispatchEvent(new Event("change"));
+    });
+};
 
 const initialize = ()=>
 {
--- admin/pages/options.php
+++ admin/pages/options.php
@@ -5,8 +5,12 @@ $option_name = "wp-api-cache";
 if ($_SERVER["REQUEST_METHOD"] === "POST")
 {
     $tmp = [];
-    foreach ($_POST as $key => $value)
-        $tmp[$key] = ["enabled"=> $value === "on"];
+    foreach ($_POST as $key => $value) {
+        if (str_ends_with($key, "_relations"))
+            $tmp[str_replace("_relations", "", $key)]["relations"] = $value;
+        else
+            $tmp[$key] = ["enabled"=> $value === "on"];
+    }
 
     update_option($option_name, json_encode($tmp));
 }
@@ -19,7 +23,13 @@ if (is_null($options))
 }
 
 $options = json_decode($options, true);
+error_log(json_encode($options));
+$post_types = get_post_types();
 ?>
+
+<script>
+const postTypes = <?php echo json_encode(array_values($post_types)); ?>;
+</script>
 
 <h1>WP API Caching settings</h1>
 <?php $rest = rest_get_server(); ?>
@@ -28,18 +38,35 @@ $options = json_decode($options, true);
     <fieldset collapsed="true">
         <legend><?php echo $namespace; ?></legend>
         <?php foreach ($rest->get_routes($namespace) as $path => $routes): ?>
+            <?php $get_requests = array_filter($routes, function($route){return isset($route["methods"]["GET"]) && $route["methods"]["GET"] === true;}); ?>
+            <!-- Only show GET requests -->
+            <?php if (count($get_requests) > 0): ?>
+            <?php $hash = base64_encode($path); ?>
+            <div>
+                <input id="<?php echo $hash; ?>" name="<?php echo $hash; ?>" <?php echo isset($options[$hash]) && $options[$hash]["enabled"] ? "checked" : ""; ?> type="checkbox" />
+                <label for="<?php echo $hash; ?>"><?php echo get_rest_url(null, $path); ?></label>
+                <fieldset class="related-types">
+                    <legend>Related post types</legend>
+                    <p>When the selected post types change the cache for the current selected route will be deleted.</p>
+                    <div data-hash="<?php echo $hash; ?>" class="related-types-dropdowns">
+                        <button type="button">Add relation</button>
 
-        <!-- Only show GET requests -->
-        <?php if (count(array_filter($routes, function($route){return isset($route["methods"]["GET"]) && $route["methods"]["GET"] === true;})) > 0): ?>
-        <?php $hash = base64_encode($path); ?>
-        <div>
-            <input id="<?php echo $hash; ?>" name="<?php echo $hash; ?>" <?php echo isset($options[$hash]) && $options[$hash]["enabled"] ? "checked" : ""; ?> type="checkbox" />
-            <label for="<?php echo $hash; ?>"><?php echo get_rest_url(null, $path); ?></label>
-        </div>
-        <?php endif; ?>
+                        <?php if (isset($options[$hash]["relations"]) && !empty($options[$hash]["relations"])): ?>
+                        <?php foreach ($options[$hash]["relations"] as $relation): ?>
+                        <select>
+                            <?php foreach ($post_types as $post_type): ?>
+                            <option <?php echo $relation === $post_type ? "selected" : ""; ?> value="<?php echo $post_type; ?>"><?php echo $post_type ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                </fieldset>
+            </div>
+            <?php endif; ?>
         <?php endforeach; ?>
     </fieldset>
     <?php endforeach; ?>
 
-    <button type="submit" class="components-button editor-post-publish-button editor-post-publish-button__button is-primary">Update</button>
+    <button type="submit">Update</button>
 </form>
